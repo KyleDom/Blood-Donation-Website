5ab17771b1a5f8c6981fc2ebf0bcf53f541c7963
jiv <jivayydelacruz@gmail.com>
2022-12-10 01:46:50 +0800

    Update Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import {Navbar} from './components/Nav'
-import { Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { Home } from './pages/Home';
-import { Admin } from './pages/Admin';
-import { BloodBank } from './pages/BloodBank';
+import React from "react";
+import { Navbar } from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Admin } from "./pages/Admin";
+import { BloodBank } from "./pages/BloodBank";
+import Home from "./pages/Home";
+import Team from "./pages/Team";
 
 function App() {
   return (
-<>
-  <Navbar/>
-  <Container className='mb-4'>
-    <Routes>
-    <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Admin />}/>
-      <Route path="/bloodbank" element={<BloodBank />}/>
-    </Routes>
-  </Container>
-</>
-  )
+    <div className="app">
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Admin />} />
+          <Route path="/bloodbank" element={<BloodBank />} />
+          <Route path="/team" element={<Team />} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,11 +1,14 @@
-import { Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
-import {NavLink} from "react-router-dom"
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs className="navbar bg-danger mb-3">
       <Container>
-        <Nav>
+        <Nav className="d-flex gap-3">
+          <h3 className="life-quest text-dark">
+            Life<span className="text-danger">Quest</span>
+          </h3>
           <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
@@ -15,8 +18,11 @@ export function Navbar() {
           <Nav.Link to="/bloodbank" as={NavLink}>
             BloodBank
           </Nav.Link>
+          <Nav.Link to="/team" as={NavLink}>
+            Team Members
+          </Nav.Link>
         </Nav>
       </Container>
     </NavbarBs>
-  )
+  );
 }
