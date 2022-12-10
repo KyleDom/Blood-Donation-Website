622f232582bcfa51fa58c8a669b2b16a65887258
jiv <jivayydelacruz@gmail.com>
2022-12-10 19:37:57 +0800

    Updated Home Page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,16 @@
-export function Home(){
-    return <h1>Home</h1>
+export function Home() {
+  return (
+    <>
+      <div className="headline">
+        <h2>Blood Donation 🩸</h2>
+        <p className="ps-5">Donate blood and Save Lives.</p>
+      </div>
+      <div className="home d-flex gap-3 align-items-center justify-content-center">
+        <button>Apply as Donor</button>
+        <button>Ask for Donor</button>
+      </div>
+    </>
+  );
 }
+
+export default Home;
